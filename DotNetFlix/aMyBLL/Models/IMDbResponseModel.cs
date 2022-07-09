963be01671bc02e3196153c7e8cfdb468ba4a857
mathhgithub <105790102+mathhgithub@users.noreply.github.com>
2022-07-09 10:55:36 +0200

    Mathias.movie dal (#3)
    * eerste commit
    
    * eerste commit

---
+++ DotNetFlix/aMyBLL/Models/IMDbResponseModel.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+namespace DotNetFlix.aMyBLL.Models;
+
+public class IMDbResponseModel
+{
+    public class MovieBLL
+    {
+        [JsonProperty(PropertyName = "rank")]
+        public string Rank { get; set; }
+
+        [JsonProperty(PropertyName = "title")]
+        public string Title { get; set; }
+
+        [JsonProperty(PropertyName = "year")]
+        public string Year { get; set; }
+
+        [JsonProperty(PropertyName = "image")]
+        public string ImageUrl { get; set; }
+
+        [JsonProperty(PropertyName = "crew")]
+        public string Crew { get; set; }
+
+    }
+
+    public class MovieRoot
+    {
+        [JsonProperty(PropertyName = "items")]
+        public List<MovieBLL> Items { get; set; }
+
+        [JsonProperty(PropertyName = "errorMessage")]
+        public string ErrorMessage { get; set; }
+    }
+
+}
