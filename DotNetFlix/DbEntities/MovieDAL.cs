986e9db1b4ff82a3ba9f29aea79eefc7e5ce7ba4
mathias <mathiash.vdab@gmail.com>
2022-07-09 08:54:51 +0200

    eerste commit

--- DotNetFlix/DbEntities/MovieDAL.cs
+++ DotNetFlix/DbEntities/MovieDAL.cs
@@ -1,6 +1,26 @@
-﻿namespace DotNetFlix.DbEntities
+﻿namespace DotNetFlix.DbEntities;
+
+public class MovieDAL
 {
-    public class MovieDAL
+    public MovieDAL(int rank, string title, int year, string imageUrl, string crew)
     {
+        Rank = rank;
+        Title = title;
+        Year = year;
+        ImageUrl = imageUrl;
+        Crew = crew;
     }
+
+    public int Id { get; set; }
+
+    // data van api
+    public int Rank { set; get; }
+    public string Title { set; get; }
+    public int Year { set; get; }
+    public string ImageUrl { get; set; }
+    public string Crew { set; get; }
+
+    // data voor features
+
+
 }
--- DotNetFlix/DbEntities/UserDAL.cs
+++ DotNetFlix/DbEntities/UserDAL.cs
@@ -2,4 +2,6 @@
 
 public class UserDAL
 {
+    public int Id { get; set; }
+
 }
